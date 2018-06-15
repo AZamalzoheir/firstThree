893dc059ca7e5eea90cec761b1de4c228e2a7fc9
Amalzoheir <A.Zamalzoheir95@gmail.com>
2018-06-16 00:39:58 +0200

    first commit

--- app/src/main/java/com/example/amalzoheir/firstthree/LoginPge.java
+++ app/src/main/java/com/example/amalzoheir/firstthree/LoginPge.java
@@ -1,13 +1,23 @@
 package com.example.amalzoheir.firstthree;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPge extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_pge);
+        loginButton=(Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPge.this, Proofile.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/amalzoheir/firstthree/Proofile.java
@@ -0,0 +1,13 @@
+package com.example.amalzoheir.firstthree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Proofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proofile);
+    }
+}
